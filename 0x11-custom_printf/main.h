414c3ccb44cecebda6c280365ca029ed65b731fa
Omiefa <bristolmelvin@yahoo.com>
2023-07-23 19:08:50 -0700

    file that contains function prototype

--- 0x11-custom_printf/main.h
+++ 0x11-custom_printf/main.h
@@ -7,12 +7,11 @@
 #include <stdlib.h>
 
 /*Custom Function Prototype*/
-int gr_putchar(char m);
+int gr_putchar(char m); 
+int gr_print_str(char *s);
 int _printf(const char *format, ...);
 int gr_print_format(char form_specifier, va_list arg_ptr);
-int gr_print_str(va_arg(arg_ptr, char *));
-int gr_print_digit(va_arg(arg_ptr, int), 10);
-int gr_print_digit(va_arg(arg_ptr, unsigned int), 16);
-int gr_print_char(va_arg(arg_ptr, int));
+int gr_print_digit(int value, int base);
 
 #endif
+
