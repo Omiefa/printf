6355aea91afb6251fc3e0a88d8f79ee9110be776
Tommy-Dan <ttuckerwra@gmail.com>
2023-07-25 15:58:24 -0700

    added file

---
+++ 0x11-custom_printf/main.h
@@ -0,0 +1,31 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdint.h>
+#include <unistd.h>
+
+
+
+struct convert
+{
+        char *id;
+        int (*f)(va_list);
+};
+typedef struct convert custom_conver_t;
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+void _vprintf(const char *format, va_list args);
+int parser(const char *format, custom_conver_t funct_list[], va_list args);
+int print_char(va_list);
+int print_string(va_list args);
+int print_percent(va_list);
+int print_unsgined_number(unsigned int num);
+int print_number(va_list);
+int print_int(va_list);
+
+
+#endif
